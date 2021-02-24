8d715afbb1ed54792ee0ef7d648274407d183472
Khornya <marine.khornya@gmail.com>
2021-02-24 09:24:20 +0100

    🔒 try to fix use of ssl

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -3,7 +3,9 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 
 export const emailTransporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
