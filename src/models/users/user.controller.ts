41302271b07dfa3286004ff40e2098913db03b80
Wilmaxys <36938212+Wilmaxys@users.noreply.github.com>
2021-03-03 17:59:27 +0100

    ✨ CUH-66  Login & refresh token routes (#13)
    Co-authored-by: Wilmaxys <wilmaxys@gmail.com>

--- src/models/users/user.controller.ts
+++ src/models/users/user.controller.ts
@@ -2,6 +2,14 @@ import UserService, { IUserCreateParams } from './user.service';
 import { Response, Request, NextFunction } from 'express';
 import { ErrorHandler } from '../../middleware/error-handler';
 import TokenService from '../tokens/token.service';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import { User } from './user.class';
+
+const getToken = (id: number) => {
+  const token = jwt.sign({ id }, process.env.APP_SECRET as string, { expiresIn: '15m' });
+  return { token, expiresAt: new Date(new Date().getTime() + 15 * 60000) };
+};
 
 export const register = (req: Request, res: Response, next: NextFunction) => {
   UserService.create(req.body as IUserCreateParams)
@@ -32,3 +40,28 @@ export const sendVerificationEmail = (req: Request, res: Response, next: NextFun
     .then(() => res.json({ message: 'Verification email sent' }))
     .catch(next)
 }
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    res.status(400).send({ success: false, message: 'Incorrect email or password' });
+  } else if (!user.emailVerifiedAt) {
+    res.status(400).send({ success: false, message: 'Email not verified' });
+  } else {
+    res.status(200).send({ success: true, message: 'Login successful', ...getToken(user.id) });
+  }
+};
+
+export const refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  const currentToken = req.headers.authorization?.split(' ')[1] as string;
+  jwt.verify(currentToken, process.env.APP_SECRET as string, (err, decoded: any) => {
+    if (err) {
+      res.status(500).send({ success: false, message: 'Server error' });
+    } else if (!decoded) {
+      res.status(500).send({ success: false, message: 'Server error' });
+    } else {
+      res.status(200).send({ success: true, message: 'Token refresh successful', ...getToken(decoded.id) });
+    }
+  });
+};
