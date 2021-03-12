f47fb99c65c52139b181df59b1e6da0f00b3b87a
PlatiScript <simonbultel@hotmail.fr>
2021-03-12 11:14:41 +0100

    CUH-34 Contacter les administrateurs de la plateforme (#16)
    * add utils component
    
    * fix issue

---
+++ src/components/utils/utils.router.ts
@@ -0,0 +1,53 @@
+import { Response, Request, NextFunction, Router } from 'express';
+import { validateRequest } from '../../middleware/validate-request';
+import joi from 'joi';
+import {sendEmail} from './utils.controller';
+
+/**
+ * Router Definition
+ */
+
+export const utilsRouter = Router();
+
+/**
+ * @swagger
+ * paths:
+ *   /utils/sendEmail:
+ *     post:
+ *       summary: Send email
+ *       tags:
+ *         - Utils
+ *       parameters:
+ *         - in: query
+ *           name: email
+ *           schema:
+ *             type: string
+ *             format: email
+ *           required: true
+ *           description: email address of the sender
+ *         - in: query
+ *           name: content
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: content of the email
+ *       responses:
+ *         '200':
+ *           description: Email sent
+ *         '400':
+ *           description: Bad request
+ */
+utilsRouter.post(
+  '/sendEmail',
+  (req: Request, res: Response, next: NextFunction) => {
+    validateRequest(
+        req,
+        next,
+        joi.object({
+          email: joi.string().email().lowercase().required(),
+          content: joi.string().required(),
+        })
+      );
+  },
+  sendEmail
+)
