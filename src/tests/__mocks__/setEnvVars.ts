9235dbfcfc01dace9139f63536b6e7efacb88208
Khornya <marine.khornya@gmail.com>
2021-04-27 17:13:36 +0200

    🚚 moved jest env variables

---
+++ src/tests/__mocks__/setEnvVars.ts
@@ -0,0 +1,17 @@
+process.env.PORT="7000"
+process.env.ENVIRONMENT="test"
+process.env.API_BASE_URL="http://example:7000"
+
+process.env.DB="database"
+process.env.DB_USER="user"
+process.env.DB_PASSWORD="somePassword"
+process.env.DB_HOST="example"
+process.env.DB_HOST_PORT="3306"
+process.env.DB_DIALECT="dialect"
+
+process.env.EMAIL_USER="contact@example.com"
+process.env.EMAIL_PASSWORD="somePassword"
+
+process.env.TOKEN_DURATION="50"
+
+process.env.APP_SECRET="MyAppSecret"
