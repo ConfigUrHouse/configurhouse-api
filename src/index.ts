7b8900b8520ed8f74162216747cc0bf3b79b67bc
Khornya <33462756+Khornya@users.noreply.github.com>
2021-06-04 19:11:02 +0200

    try to fix cors

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,7 @@ declare const module: WebpackHotModule;
   app.use((req, res, next) => {
     next();
   }, cors());
+  app.use(cors());
   app.use(express.json());
   app.use('/public', express.static('public'));
 
