c66ff5bbd0502543ef52102782d7dc9192c66944
Khornya <33462756+Khornya@users.noreply.github.com>
2021-07-14 10:28:48 +0200

    🥅 try to fix email errors

--- src/components/utils/utils.controller.ts
+++ src/components/utils/utils.controller.ts
@@ -1,14 +1,19 @@
 import { Response, Request, NextFunction } from 'express';
+import { ErrorHandler } from '../../middleware/error-handler';
 import UtilsService from './utils.service';
 
 export const sendEmail = (req: Request, res: Response, next: NextFunction) => {
   UtilsService.sendEmail(req.body.email as string, req.body.content as string, req.body.subject as string)
     .then(() => res.json({ message: 'Email sent' }))
-    .catch(next);
+    .catch((err: any) => {
+      next(new ErrorHandler(500, err));
+    });
 };
 
 export const sendEmails = (req: Request, res: Response, next: NextFunction) => {
   UtilsService.sendEmails(req.body.emails as string[], req.body.subject as string, req.body.content as string)
     .then(() => res.json({ message: 'Emails sent' }))
-    .catch(next);
+    .catch((err: any) => {
+      next(new ErrorHandler(500, err));
+    });
 };
